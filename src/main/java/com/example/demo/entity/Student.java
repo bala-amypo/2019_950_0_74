3f20a64d7c060fa1e9cc08b40dc107dfda2cf022
bala-amypo <bala@amypo.in>
2025-12-17 05:14:41 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -4,5 +4,17 @@ public class Student{
     private int id;
     private String name;
     private String email;
-    
+
+}
+public int getid(){
+    return id;
+
 } 
+public void setId(int id){
+    this.id=id;
+}
+public void getName(String name){
+    return name;
+
+}
+public Str
